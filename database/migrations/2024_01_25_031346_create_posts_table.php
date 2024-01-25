c3c3d3c46972fefca820adbf4e2a209f54dc80fe
PalminiStephane <palministephane@gmail.com>
2024-01-25 10:45:10 +0100

    Fix: migrations

--- database/migrations/2024_01_25_031346_create_posts_table.php
+++ database/migrations/2024_01_25_031346_create_posts_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('slug')->unique();
+            $table->string('title', 100);
+            $table->string('slug',64)->unique();
             $table->string('excerpt');
             $table->text('content');
             $table->string('thumbnail');
