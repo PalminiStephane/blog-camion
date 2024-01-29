156bae67be5b528595de856120dc5c8a0252aa25
PalminiStephane <palministephane@gmail.com>
2024-01-29 22:21:56 +0100

    Filtrer les posts par catégorie ou par tag

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Category;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -15,6 +17,25 @@ class PostController extends Controller
       ]);
     }
 
+    public function postByCategory(Category $category): View
+    {
+      return view('posts.index', [
+        // 'posts' => $category->posts()->latest()->paginate(10),
+        'posts' => Post::where(
+          'category_id', $category->id
+          )->latest()->paginate(10),
+      ]);
+    }
+
+    public function postByTag (Tag $tag): View
+    {
+      return view('posts.index', [
+        // 'posts' => $tag->posts()->latest()->paginate(10),
+        'posts' => Post::whereRelation(
+          'tags', 'tags.id', $tag->id
+          )->latest()->paginate(10),
+      ]);
+    }
     public function show(Post $post): View
     {
       return view('posts.show', [
